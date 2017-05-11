5512843b99ae8a872975f7b6ed21e78ff40269b6
Angelo Ruwantha <ruwanthawin32@gmail.com>
2017-05-11 18:41:35 +0530

    Update Program.cs

--- DnsExfiltration/Program.cs
+++ DnsExfiltration/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Text;
 using System.Threading;
 
+//Angelo Runwatha aka @h3llwings 
+//https://h3llwings.wordpress.com
+
 namespace DnsExfiltration
 {
     class Program
